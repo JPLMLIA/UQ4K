e66e93f7fa85723b572a892ee74315fb53933639
Mostafa Samir <mostafa.3210@gmail.com>
2022-01-06 15:55:25 +0200

    finalize GdOpt implementation

--- uq4k/gradient/gd_optimizer.py
+++ uq4k/gradient/gd_optimizer.py
@@ -4,6 +4,7 @@
 # Written  : December 29, 2021
 # Last Mod : December 29, 2021
 
+from functools import partial
 from typing import List, Tuple, Union
 
 import cvxpy as cp
@@ -14,6 +15,7 @@ import numpy as np
 import optax
 from scipy import stats
 
+from uq4k.gradient.early_stopper import EarlyStopper
 from uq4k.models.loss import DifferentaibleMeritFunc
 
 
@@ -66,7 +68,12 @@ class GdOpt:
         return mle_error + delta
 
     def find_mle(
-        self, initial_theta: jax.numpy.DeviceArray, max_epoch: int = 1000, lr: float = 0.01
+        self,
+        initial_theta: jax.numpy.DeviceArray,
+        max_epoch: int = 1000,
+        lr: float = 0.01,
+        min_improvement: float = 0.01,
+        patience: int = 1000,
     ) -> Tuple[jax.numpy.DeviceArray, jax.numpy.DeviceArray]:
         """retrieves the MLE solution by applying gradient descent
 
@@ -78,6 +85,12 @@ class GdOpt:
             the number of epochs to run GD for, by default 1000
         lr : float, optional
             the learning rate, by default 0.01
+        min_improvement: float, optional
+            the relative minimum difference between loss to consider an improvement,
+            default to 0.1 (10% lower than the smallest observed loss)
+        patiance: int, optional
+            the number of epochs to wait for improvement in the loss before stopping
+
 
         Returns
         -------
@@ -85,6 +98,9 @@ class GdOpt:
             - MLE estimate of theta
             - value of MLE error
         """
+        early_stopper = EarlyStopper(
+            min_improvement=min_improvement, patience=patience, improvement_type="relative"
+        )
         optimizer = optax.adam(learning_rate=lr)
         state = optimizer.init(initial_theta)
         theta = initial_theta
@@ -97,10 +113,76 @@ class GdOpt:
             new_theta = optax.apply_updates(theta, updates)
             return error, new_theta, new_state
 
-        for _ in range(max_epoch):
+        for i in range(max_epoch):
             error, theta, state = update_step(theta, state)
+            _, stop = early_stopper.check(error, i)
+            if stop:
+                break
 
         return theta, error
+
+    def __get_furthest_point(
+        self,
+        initial_theta: jax.numpy.DeviceArray,
+        M_alpha: float,
+        center: np.ndarray,
+        max_epoch: int,
+        lr: float,
+        min_improvement: float = 0.01,
+        patience: int = 1000,
+    ) -> jax.numpy.DeviceArray:
+        """performs gradient descent on the whole objective function to get the furthest point from the center
+
+        Parameters
+        ----------
+        initial_theta: jax.numpy.DeviceArray
+            the initial value of theta to start from
+        M_alpha : float
+            the slack defining the ellipsoid from the likelihood ratio
+        center : np.ndarray
+            the center to find the furthest point from
+        max_epoch : int
+            max number of epoch to run the optimization for
+        lr : float
+            the learning rate used
+        min_improvement: float, optional
+            the relative minimum difference between loss to consider an improvement,
+            default to 0.1 (10% lower than the smallest observed loss)
+        patiance: int, optional
+            the number of epochs to wait for improvement in the loss before stopping
+
+        Returns
+        -------
+        jax.numpy.DeviceArray
+            the furthest point found
+        """
+        early_stopper = EarlyStopper(
+            min_improvement=min_improvement, patience=patience, improvement_type='relative'
+        )
+        optimizer = optax.adam(learning_rate=lr)
+        state = optimizer.init(initial_theta)
+        theta = initial_theta
+
+        bound_objective = partial(self.objective, center=center, M_alpha=M_alpha)
+
+        furthest_point = None
+
+        @jax.jit
+        def update_step(theta, state):
+            loss, grad = jax.value_and_grad(bound_objective)(theta)
+            updates, new_state = optimizer.update(grad, state)
+            new_theta = optax.apply_updates(theta, updates)
+            return loss, new_theta, new_state
+
+        for i in range(max_epoch):
+            loss, theta, state = update_step(theta, state)
+            improvement, stop = early_stopper.check(loss, i)
+            if stop:
+                break
+            if improvement:
+                furthest_point = jax.lax.stop_gradient(self.objective.qoi_func(theta))
+
+        return furthest_point
 
     def optimize_min_e_ball(
         self,
@@ -110,6 +192,7 @@ class GdOpt:
         raduis_eps: float,
         conf_level: float,
         max_epoch: int = 100000,
+        lr: float = 0.001,
         man_delta: Union[None, float] = None,
         bounds=None,
     ) -> Tuple[np.ndarray, float, List[np.ndarray], np.ndarray, float]:
@@ -129,6 +212,8 @@ class GdOpt:
             confidence level usded in chi-squared calculation of M_alpha
         max_epoch : int, optional
             the maximum number of epochs to run GD for, by default 100000
+        lr: float, optional
+            the learning rate to be used
         man_delta : Union[None, float], optional
             manual value of delta bypassing the chi-squared method of M_alpha, by default None
         bounds : [type], optional
@@ -143,4 +228,80 @@ class GdOpt:
             - center: the center of the minimum enclosing ball
             - raduis: the raduis of the minimum enclosing ball
         """
-        pass
+        dims = initial_theta.size
+
+        S = []
+        center = None
+        raduis = 0
+
+        mle_theta, mle_error = self.find_mle(initial_theta, max_epoch, lr)
+
+        M_alpha = self.compute_M_alpha(
+            sigma_2, mle_error, df=dims, conf_level=conf_level, man_delta=man_delta
+        )
+
+        center = np.asarray(jax.lax.stop_gradient(self.objective.qoi_func(mle_theta)))
+        S.append(center)
+
+        raduis_diff = np.inf
+
+        while raduis_diff > raduis_eps:
+            furthest_point = self.__get_furthest_point(initial_theta, M_alpha, center, max_epoch, lr)
+            furthest_point = np.asarray(furthest_point)
+
+            S.append(furthest_point)
+            center, r_squared = mb.get_bounding_ball(np.array(S).reshape(-1, 1))
+
+            raduis_diff = np.abs(np.sqrt(r_squared) - raduis)
+            raduis = np.sqrt(r_squared)
+
+            if len(S) > dims + 1:
+                distances = [np.linalg.norm(center - Si) for Si in S]
+                remove_indx = np.argmin(distances)
+                S.pop(remove_indx)
+
+        return mle_theta, M_alpha, np.array(S), center, raduis
+
+    def weight_optimization(self, S):
+        """
+        Find dirac weights after min enclosing ball opt
+
+        Parameters:
+            S (np arr) : n x m, n - num diracs | m - dim each dirac
+
+        Returns:
+            optimized weights over diracs (n,) numpy array
+        """
+        # find the optimization objects
+        ONE_D = len(S.shape) == 1
+        if ONE_D:
+            n = S.shape[0]
+
+        else:
+            n, m = S.shape
+
+        Q_mat = np.zeros(shape=(n, n))
+
+        if ONE_D:
+            Q_mat = np.outer(S, S)
+            v = np.square(S)
+
+        else:
+            for t in range(m):
+                Q_mat += np.outer(S[:, t], S[:, t])
+
+            v = np.square(S).sum(axis=1)
+
+        # perform the optimization
+        p_vec = cp.Variable(n)
+
+        problem = cp.Problem(
+            objective=cp.Minimize(cp.quad_form(p_vec, Q_mat) - v.T @ p_vec),
+            constraints=[p_vec >= np.zeros(n), cp.sum(p_vec) == 1],
+        )
+
+        # solve and check convergence
+        problem.solve()
+        assert problem.status == 'optimal'
+
+        return p_vec.value
